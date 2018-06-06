b1a4abe8cf7217382bcc08d4429dd9c4c5b01735
Daniel Onggunhao <dan@daniel.ai>
2018-06-06 16:44:05 +0800

    Iterate through all historical blocks in the Ethereum blockchain

--- quantify.py
+++ quantify.py
@@ -1,10 +1,18 @@
 from web3 import Web3, HTTPProvider
+import json
 
 # Geth node parameters
 rpcport = '9111'
 
+# Experiment parameters
+numBuckets = 50
+
 # Instantiate web3
 web3 = Web3(HTTPProvider('http://localhost:' + rpcport))
 
-# Testing code
-print web3.eth.getBlock('latest').number
+# Iterate over blocks and print transactions
+latest = web3.eth.getBlock('latest').number
+
+for i in range(4400000, 4500000):
+	currentBlock = web3.eth.getBlock(i, full_transactions=True)
+	print currentBlock.transactions
